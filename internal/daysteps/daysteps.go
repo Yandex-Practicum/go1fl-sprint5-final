67f885c8930d41b5a493ca1738b387ae595d8172
Dmitrii Bibiluri <dmitri.bibiluri@gmail.com>
2025-11-12 17:02:24 +0300

    finished creating packages trainings and daysteps

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,59 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps int
+	Duration time.Duration
+	personaldata.Personal
 }
 
-func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+func (ds *DaySteps) Parse(datastring string) (err error) {	
+	parts := strings.Split(datastring, ",")
+    if len(parts) != 2 {
+        return errors.New("wrong amount of input data")
+    }
+
+    stepsStr := strings.TrimSpace(parts[0])
+    durationStr  := strings.TrimSpace(parts[1])
+
+    steps, err := strconv.Atoi(stepsStr)
+    if err != nil {
+        return errors.New("failed to convert steps to int: " + err.Error())
+    }
+    if steps < 0 {
+        return errors.New("steps must be >= 0")
+    }
+    ds.Steps = steps
+
+    d, err := time.ParseDuration(durationStr)
+    if err != nil {
+        return errors.New("failed to parse duration: " + err.Error())
+    }
+    if d <= 0 {
+        return errors.New("duration must be > 0")
+    }
+    ds.Duration = d
+
+    return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+    dist := spentenergy.Distance(ds.Steps, ds.Personal.Height)
+    cals, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Personal.Weight, ds.Personal.Height, ds.Duration)
+    if err != nil {
+        return "", err
+    }
+    res := fmt.Sprintf(
+        "Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", ds.Steps, dist, cals)
+    return res, nil
 }
