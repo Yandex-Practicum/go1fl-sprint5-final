f4f79196a55090be52b9bc4320399f7f28c0e61e
Qace <ya.kukin2018@yandex.ru>
2026-04-08 23:31:16 +0700

    first

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,61 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	s := strings.Split(datastring, ",")
+
+	if len(s) != 2 {
+		return fmt.Errorf("неверный формат данных: %s", datastring)
+	}
+
+	ds.Steps, err = strconv.Atoi(s[0])
+	if err != nil {
+		return err
+	}
+
+	ds.Duration, err = time.ParseDuration(s[1])
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, 0)
+	if err != nil {
+		return "", err
+	}
+
+	result := fmt.Sprintf(
+		"Количество шагов: %d.\n"+
+			"Дистанция составила %.2f км.\n"+
+			"Вы сожгли %.2f ккал.",
+		ds.Steps,
+		distance,
+		calories,
+	)
+
+	return result, nil
+
 }
