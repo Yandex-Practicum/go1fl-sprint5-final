39e7c9aad8291c194ab061b823e2c823e73f4218
Emil Dehermenzhi <emilpixel4@gmail.com>
2026-04-21 22:22:58 +0300

    Sprint 5 solution

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,76 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
-func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+func (ds *DaySteps) Parse(datastring string) error {
+	if datastring == "" {
+		return errors.New("empty input")
+	}
+
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("invalid format")
+	}
+
+	// steps
+	stepsStr := parts[0]
+	if strings.HasPrefix(stepsStr, "+") {
+		stepsStr = strings.TrimPrefix(stepsStr, "+")
+	}
+
+	steps, err := strconv.Atoi(stepsStr)
+	if err != nil || steps <= 0 {
+		return errors.New("invalid steps")
+	}
+
+	// duration
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil || duration <= 0 {
+		return errors.New("invalid duration")
+	}
+
+	ds.Steps = steps
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	if ds.Steps <= 0 || ds.Duration <= 0 || ds.Weight <= 0 || ds.Height <= 0 {
+		return "", errors.New("invalid data")
+	}
+
+	dist := spentenergy.Distance(ds.Steps, ds.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(
+		ds.Steps,
+		ds.Weight,
+		ds.Height,
+		ds.Duration,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps,
+		dist,
+		calories,
+	), nil
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,29 +1,56 @@
 package spentenergy
 
 import (
+	"fmt"
 	"time"
 )
 
-// Основные константы, необходимые для расчетов.
 const (
-	mInKm                      = 1000 // количество метров в километре.
-	minInH                     = 60   // количество минут в часе.
-	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
-	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
+	mInKm                      = 1000
+	minInH                     = 60
+	stepLengthCoefficient      = 0.45
+	walkingCaloriesCoefficient = 0.5
 )
 
-func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
-}
+func Distance(steps int, height float64) float64 {
+	if steps <= 0 || height <= 0 {
+		return 0
+	}
 
-func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	return float64(steps) * stepLength / mInKm
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 || duration <= 0 {
+		return 0
+	}
+
+	dist := Distance(steps, height)
+	return dist / duration.Hours()
 }
 
-func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("invalid input data")
+	}
+
+	speed := MeanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+
+	return (weight * speed * minutes) / minInH, nil
+}
+
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("invalid input data")
+	}
+
+	speed := MeanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+
+	calories := (weight * speed * minutes) / minInH
+	calories *= walkingCaloriesCoefficient
+
+	return calories, nil
 }
